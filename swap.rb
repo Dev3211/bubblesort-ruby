2b424fc1c527c4e6032b9327970750755ccac5e4
Dev3211 <devsarma321@gmail.com>
2018-09-29 19:12:28 +0530

    Create swap.rb

---
+++ swap.rb
@@ -0,0 +1,25 @@
+def bubblesort(arr)
+check = arr.kind_of?(Array)
+
+if (!check)
+  return "Please make sure you are passing an array"
+else
+test = arr.length
+
+for i in 0..test - 1
+    for j in 0..test - 2 - i
+     if arr[j] > arr[j + 1]
+      temp = arr[j]
+      arr[j] = arr[j + 1]
+      arr[j + 1] = temp
+      end
+   end
+end
+
+end
+puts arr.inspect
+end
+
+array = [8, 8, 5, 3, 4]
+
+bubblesort(array)
