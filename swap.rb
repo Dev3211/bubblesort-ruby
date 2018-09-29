3f053ade3c5700ee647855eeebfa5c2dd82c0c43
Dev3211 <devsarma321@gmail.com>
2018-09-29 19:14:54 +0530

    make it look good

--- swap.rb
+++ swap.rb
@@ -2,21 +2,21 @@ def bubblesort(arr)
 check = arr.kind_of?(Array)
 
 if (!check)
-  return "Please make sure you are passing an array"
+return "Please make sure you are passing an array"
 else
 test = arr.length
 
 for i in 0..test - 1
     for j in 0..test - 2 - i
-     if arr[j] > arr[j + 1]
-      temp = arr[j]
-      arr[j] = arr[j + 1]
-      arr[j + 1] = temp
+      if arr[j] > arr[j + 1]
+         temp = arr[j]
+         arr[j] = arr[j + 1]
+         arr[j + 1] = temp
+         end
       end
    end
 end
 
-end
 puts arr.inspect
 end
 
